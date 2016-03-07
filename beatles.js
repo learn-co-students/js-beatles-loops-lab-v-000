0e28cc73b5d2deb008d68e633c825c5cbd1981e9
Kevin McCormack <wittyman37@yahoo.com>
2016-03-06 19:04:00 -0500

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+
+  return array;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+
+  while (i < facts.length) {
+    array.push(facts[i]+"!!!");
+    i += 1;
+  }
+
+  return array;
+}
+
+
+function iLoveTheBeatles(num) {
+  var array = [];
+
+  do {
+    array.push("I love the Beatles!");
+    num += 1;
+  } while (num < 15);
+
+  return array;
+}
