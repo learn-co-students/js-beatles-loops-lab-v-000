b12093196ff8f2bf447aa62f6c075480340edb22
empireofryan <rjohnson2001@gmail.com>
2016-03-22 23:48:57 -0400

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function theBeatlesPlay(musiciansArr, instrumentsArr){
+  var sentences = [];
+  for(var i = 0; i < musiciansArr.length; i++){
+    sentences.push(musiciansArr[i] + " plays " + instrumentsArr[i]);
+  }
+  return sentences;
+}
+
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var exclamations = [];
+  var i = 0;
+  while (i < facts.length){
+    exclamations.push(facts[i] + "!!!");
+    i++;
+  }
+  return exclamations;
+}
+
+function iLoveTheBeatles(num){
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return love;
+}
