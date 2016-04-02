0de943f9c4f1291d215d4fd70fcad832c334ea86
esimonian <eli.simonian@gmail.com>
2016-04-01 22:52:35 -0700

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function theBeatlesPlay(players, instruments) {
+ var arr = []
+ for (var i = 0; i < players.length; i++) {
+   arr.push(players[i] + " plays " + instruments[i])
+ }
+ return arr
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var answers = []
+  for (var i in facts) {
+    answers.push(facts[i] + "!!!")
+  }
+ return answers
+}
+
+function iLoveTheBeatles(num) {
+  var answer = []
+  do {
+    answer.push("I love the Beatles!")
+    num++
+  } while (num < 15)
+
+return  answer
+}
