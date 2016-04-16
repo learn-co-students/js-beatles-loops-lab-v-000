e4465eae1934c1d8179d1bb6fffea326548ca2fa
jjlippman <jjlippman@earthlink.net>
2016-04-16 14:51:24 -0600

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function theBeatlesPlay(mus, intr){
+  var sentences = [];
+    for(var i = 0; i<mus.length; i++){
+      sentences.push(mus[i]  + " plays " + intr[i]);
+    }
+    return sentences;
+}
+
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var blah = [];
+  var i=0;
+  while(i<facts.length){
+    blah.push(facts[i] + "!!!");
+    i++;
+  }
+  return blah;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  
+  do{
+    
+    arr.push("I love the Beatles!");
+    num++;
+  } while(num<15);
+
+  return arr;
+
+}
