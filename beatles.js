35474443c5148f5fda85f4735751cd7255861de2
the-widget <brennenawana108ny@gmail.com>
2016-04-30 22:09:53 -0400

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,28 @@
-'use strict';
+function theBeatlesPlay(name, instr) {
+  bandmates = [];
+  for (var i = 0; i <= 3; i++) {
+    sentence = name[i] + " plays " + instr[i]
+    bandmates.push(sentence);
+  }
+  return bandmates;
+}
+
+function johnLennonFacts() {
+  var num = 0;
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var likeWhoa = [];
+  while (facts[num] !== undefined) {
+    likeWhoa.push(facts[num].concat("!!!"));
+    num++;
+  }
+  return likeWhoa
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    num++;
+  } while (num < 15)
+  return array;
+}
