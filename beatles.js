84c9b155509a7a5d07c41888ec66c1a720d11852
cernanb <cernanb@gmail.com>
2016-05-02 15:05:27 -0700

    created loops in javascript

--- beatles.js
+++ beatles.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  // musicians.forEach(function(musician, index){
+  //   debugger;
+  // });
+  for (var i = 0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
+
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newArray = [];
+  var i = 0;
+  while (i < facts.length) {
+    newArray.push(facts[i] + "!!!")
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num){
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return newArray;
+}
