83d17f8e748b5d497689c1d854e4f15a8332cbc4
rocketNeck <homestarflyer@gmail.com>
2016-05-02 18:56:29 -0400

    Done.

--- beatles.js
+++ beatles.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var excitedFacts = [];
+  var i = 0;
+  while (i < facts.length){
+    excitedFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return excitedFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  var i = num;
+  do {
+    love.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return love;
+}
