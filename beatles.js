cbe94c43b07febae27b7652e2388740ed88de361
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-11 17:41:17 -0400

    Done.

---
+++ beatles.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  const strings = [];
+
+  for (var i = 0 ; i < musicians.length; i++) {
+    strings.push(musicians[i]+" plays "+instruments[i]);
+  };
+  return strings;
+}
+
+function johnLennonFacts(facts) {
+  counter = 0
+  
+  while (counter < facts.length) {
+    facts[counter] = facts[counter]+"!!!";
+    counter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  const strings = [];
+
+    do {
+    strings.push("I love the Beatles!");  
+    num++;
+  } while (num < 15);
+
+  return strings;
+}
