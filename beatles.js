6db7f3529335e1cb695b43ea2d73b88a42b25243
hudbuz <hzb2@cornell.edu>
2016-07-13 17:29:04 -0400

    Done.

---
+++ beatles.js
@@ -0,0 +1,36 @@
+
+
+function theBeatlesPlay(artists, instruments) {
+
+
+  var strings = [];
+
+  for (var i = 0; i < artists.length; i++) {
+    strings.push(artists[i] + " plays the " + instruments[i]);
+   
+  };
+  return strings;
+
+}
+
+
+function johnLennonFacts(array) {
+  var counter = array.length;
+  while (counter > 0) {
+    array[counter - 1] += "!!!";
+    counter -= 1;
+  }
+  return array;
+}
+
+
+function iLoveTheBeatles(number) {
+  var array = []
+  var counter = number 
+  do {
+    array.push("I love the Beatles!");
+    counter -= 1
+  }
+  while (counter > 0)
+    return array;
+}
