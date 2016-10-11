14d0d2fd3656931facb4c12bd6ba19f131d314ca
Christian Whitesides <whitesidescr@gmail.com>
2016-10-11 17:41:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 function theBeatlesPlay(musicians, instruments) {
-  var musiciansAndTheirInstruments = [];
+  const musiciansAndTheirInstruments = [];
+
   for(let i = 0; i < musicians.length; i++) {
-    var string = `${musicians[i]} plays ${instruments[i]}`;
-    musiciansAndTheirInstruments.push(string);
+    musiciansAndTheirInstruments.push(`${musicians[i]} plays ${instruments[i]}`);
   }
 
   return musiciansAndTheirInstruments;
 }
 
 function johnLennonFacts(facts){
-  var i = 0;
+  let i = 0;
+
   while(i < facts.length) {
     facts[i] = facts[i] + "!!!";
-    console.log(i++)
+    i++;
   }
 
   return facts;
 }
 
 function iLoveTheBeatles(num) {
-  var beatlesLove = [];
+  const beatlesLove = [];
 
   do {
     beatlesLove.push("I love the Beatles!");
-    console.log(++num);
+    num++;
   } while (num < 15);
 
   return beatlesLove;
 }
+
+// A good look at the WHY of using `let`, `const`, and `var`.
+// https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.gyp6ps7o3
