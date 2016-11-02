65edc94f09d83ce785bc2c334e2088334050244e
karuna24s <karuna24s@gmail.com>
2016-11-02 13:33:27 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesPlay = [];
+  for (var i= 0; i < musicians.length; i++) {
+    var string = musicians[i] + " plays " + instruments[i];
+    beatlesPlay.push(string);
+  }
+  return beatlesPlay;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newArray = [];
+  while (i < facts.length) {
+    newArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return newArray;
+}
+
+// function johnLennonFacts(facts) {
+//   for (var i = 0; i < facts.length; i++) {
+//     facts[i] += "!!!";
+//   }
+//   return facts;
+// }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  }
+  while (number < 15); 
+  return array;
+}
