5edb0fc839bbe843bc8789a650da657f6ef77a13
wook kim <wooke2e@gmail.com>
2017-05-15 15:03:43 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var array = [];
-
-  
+  do {
+    array.push("I love the Beatles!")
+  } while (++number < 15);
+  return array
 };
