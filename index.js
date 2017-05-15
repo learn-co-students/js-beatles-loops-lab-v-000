24b8722409be4a568bb84d19a81ba6045295a408
wook kim <wooke2e@gmail.com>
2017-05-15 12:21:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (var i = 0, i < 4, i++){
+  for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
-  }  
+  }
   return array
 };
+
+function johnLennonFacts(facts){
+  var i = facts.length;
+  var a = 0;
+  var array = [];
+  while (i > a){
+    array.push(facts[a++] + "!!!")
+  }
+  return array
+};
+
+function iLoveTheBeatles(){
+  
+}
