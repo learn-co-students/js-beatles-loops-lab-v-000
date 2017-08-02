0ff3ed1af35e3f1a52dd9cf3994b7813b3e669e3
Brad Carlson <b2carlso@yahoo.com>
2017-08-02 18:40:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var lyrics = [];
+  for (var i = 0; i < musicians.length; i++) {
+    lyrics.push(musicians[i] + " plays " + instruments[i]);
+  }
+ return lyrics;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1
+  } while (number < 15);
+  return array;
+}
