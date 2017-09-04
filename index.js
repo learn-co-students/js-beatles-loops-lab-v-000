6d7185e530f3939b6797a1895c0b4e8e732b6c52
Emmanuel Owusu <e_owusu@live.com>
2017-09-04 21:12:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = [];
+  var array = []
+
   for(var i=0; i<musicians.length; i++){
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return array;
+  return array
 }
 
+
 function johnLennonFacts(facts){
-  const newArray = [];
+  const newArray = []
   let i = 0;
   while(i<facts.length){
-    newArray.push(`${facts[i]}!!!`);
+    newArray.push(`${facts[i]}!!!`)
     i++
   }
-  return newArray;
+  return newArray
 }
 
 function iLoveTheBeatles(number){
-  var array = [];
-  do{
-    array.push("I love the Beatles!")
-    number++
-  }
-  while(number < 15);
-  return array
+  var otherArray = []
+do{
+  otherArray.push("I love the Beatles!")
+  number++
+}
+while(number < 15)
+  return otherArray
 }
