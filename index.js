f55e8b29e13ed12a9b6bf6ab5b42f017d1d7fa78
smithwebtek <brad@smithwebtek.com>
2018-05-15 13:41:40 -0400

    Add another way

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-function theBeatlesPlay(musicians, instruments) {
-	var ary = [];
-	var i = 0;
-	for (i = 0; i < musicians.length; i++) {
-		ary.push(musicians[i] + " plays " + instruments[i]);
+// function theBeatlesPlay(musicians, instruments) {
+// 	var ary = [];
+// 	for (i = 0; i < musicians.length; i++) {
+// 		ary.push(musicians[i] + " plays " + instruments[i]);
+// 	}
+// 	return ary;
+// }
+
+// function theBeatlesPlay(musicians, instruments) {
+// 	return musicians.map((m, i) => {
+// 		return m + ' plays ' + instruments[i]
+// 	})
+// }
+
+function theBeatlesPlay(arraym, arrayi) {
+	var arrays = []
+	for (let i = 0; i < arraym.length; i++) {
+		arrays.push(`${arraym[i]} plays ${arrayi[i]}`)
 	}
-	return ary;
+	return arrays
+
 }
 
-function johnLennonFacts(ary) {
-	var l = ary.length;
+
+
+
+// function johnLennonFacts(ary) {
+// 	var l = ary.length;
+// 	var i = 0;
+// 	var newAry = [];
+// 	while (i < ary.length) {
+// 		newAry.push(ary[i] + "!!!");
+// 		i++;
+// 	}
+// 	return newAry;
+// }
+
+function johnLennonFacts(arr) {
 	var i = 0;
-	var newAry = [];
-	while (i < ary.length) {
-		newAry.push(ary[i] + "!!!");
+	while (i < arr.length) {
+		arr[i] += "!!!";
 		i++;
 	}
-	return newAry;
+	return arr;
 }
 
 function iLoveTheBeatles(num) {
